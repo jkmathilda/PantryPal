6b6b3b647df6ca8cc4ae90c8b4918dd7e3ad882a
Mathilda Lee <jkmathilda627@gmail.com>
2024-01-08 21:10:37 -0500

    Update toRecipe.py

--- pages/toRecipe.py
+++ pages/toRecipe.py
@@ -3,15 +3,17 @@ from flask import Flask, request, render_template
 from dotenv import load_dotenv
 
 def provide_recipes():
-    ingredients = [egg, flour]
+    ingredients = ["egg", "flour", "sugar", "olive oil", "carrots", "onions", "salt", "pepper"]
     # User input
     
     prompt_template = PromptTemplate.from_template(
-        '''Provide a list of dishes you can cook with the provided ingredients. Do not provide any dishes that requires different
-        ingredients: {ingredients}'''
+        '''Provide a list of recipes you can cook with the provided ingredients. Do not provide any dishes that require
+        ingredients other than: {ingredients} However, you are allowed to provide a recipe that doesn't use all ingredients. '''
         # Might need to add basic ingredients manually: ex. water
     )
-    prompt_template.format(ingredients=ingredients)
+    recipes = prompt_template.format(ingredients=ingredients)
+    
+    return recipes
 
 def main():
     # Make user's api key work instead of our personal key
