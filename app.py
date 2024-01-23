0c8ed9706150129a1395e1ef2caa460616325e84
jkmathilda <jkmathilda627@gmail.com>
2024-01-23 18:56:55 -0500

    Update all inclusively

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import toRecipe
 
 app = Flask(__name__)
 
@@ -8,25 +9,52 @@ app = Flask(__name__)
 def home():
    return render_template('home.html')
 
-@app.route('/toRecipe')
+
+@app.route('/toRecipe', methods = ["GET", "POST"])
 def recipe():
-   return render_template('toRecipe.html')
-
-@app.route('/ingredients')
-def ingredients():
-   return render_template('ingredients.html')
-
-@app.route('/search', methods = ["GET", "POST"])
-def search():
+    
     if request.method == 'POST':
         form_data = request.form
-        val1 = form_data['inputContent']
-        val2 = form_data.get('butter')  # is_checked
-        return (f' inputContent: {val1}<br>button(butter) is checked: {val2}')
-    else:
-        # return render_template('toRecipe.html')
-        return 'This is Search Page!'
+        ingredients = form_data['inputContent']
+        staples = ""
+        if form_data.get('butter'):
+            staples += "butter,"
+        if form_data.get('oil'):
+            staples += "oil,"
+        if form_data.get('water'):
+            staples += "water,"
+        if form_data.get('salt'):
+            staples += "salt,"
+        if form_data.get('pepper'):
+            staples += "pepper"
+            
+        
+        recipes = toRecipe.provide_recipes(ingredients, staples)
+    
+    
+        return render_template(
+            'toRecipe.html', 
+            recipe_name=recipes
+        )
+        
+    else: 
+        return render_template('toRecipe.html')
+
+
+@app.route('/pantryTracker')
+def ingredients():
+   return render_template('pantryTracker.html')
+
+# @app.route('/search', methods = ["GET", "POST"])
+# def search():
+#     if request.method == 'POST':
+#         form_data = request.form
+#         val1 = form_data['inputContent']
+#         val2 = form_data.get('butter')  # is_checked
+#         return (f' inputContent: {val1}<br>button(butter) is checked: {val2}')
+#     else:
+#         # return render_template('toRecipe.html')
+#         return 'This is Search Page!'
 
 if __name__ == '__main__':
-    # app.run('0.0.0.0', port=5000, debug=True)
     app.run('0.0.0.0', port=80, debug=True)
--- toRecipe.py
+++ toRecipe.py
@@ -1,57 +1,15 @@
-from flask import Flask, request, render_template
 from dotenv import load_dotenv
 import os
 from langchain.prompts import PromptTemplate
 from langchain_openai import OpenAI
 from langchain.chains import LLMChain
 from langchain_community.utilities.dalle_image_generator import DallEAPIWrapper
- 
-
-app = Flask(__name__, template_folder='../templates', static_folder='../static')
-data = ""
-
-@app.route("/", methods=['GET'])
-def root():
-    main()
-    return render_template("index.html")
 
 
-@app.route("/", methods=['POST'])
-def second_post():
-    global data
-    searchText = request.form['inputText'] 
-    staples = ""
-    if request.form.get('butter'):
-        staples = staples + "butter"
-    if request.form.get('oil'):
-        if staples == "": staples = staples + "oil"
-        else: staples = staples + ", oil"
-    if request.form.get('water'):
-        if staples == "": staples = staples + "water"
-        else: staples = staples + ", water"
-    if request.form.get('salt'):
-        if staples == "": staples = staples + "salt"
-        else: staples = staples + ", salt"
-    if request.form.get('pepper'):
-        if staples == "": staples = staples + "pepper"
-        else: staples = staples + ", pepper"
-    data = provide_recipes(searchText, staples)
-    # return render_template("second.html") #, *res* = data)
-
-# placeholder for second html page, incase we want to update it
-# @app.route("/second", methods=['GET', 'POST'])
-# def second():
- #   global data
- #   return render_template("second.html") #, *res* = data)
 
 def provide_recipes(ingredients, staples):
     api_key = os.getenv("OPENAI_API_KEY")
     llm = OpenAI(openai_api_key=api_key)
-    
-    # User input
-    #ingredients = "egg, flour, olive oil, water, garlic, onion, salt, pepper"
-    # Checkbox
-    #staples = "oil, butter, salt"
     
     prompt_template = PromptTemplate.from_template(
         '''Provide recipes (max 21 recipes) for 2 servings you can cook with the provided ingredients. Do not provide any dishes that require
@@ -142,31 +100,31 @@ def main():
         # Display on screen? 
         # Fail to login and unable to use the program/website
         
-    # Take ingredients, staples from the user inputs
-    ingredients, staples = # user input
-    recipes = provide_recipes(ingredients, staples)
-    lor = recipes.split('RECIPE NAME: ') # list of recipes
-    # List Of Recipe Names, List of INGRedients, List Of Processes
-    lorn, lingr, lop = [], [], []
-    for i in range(len(lor)):
-        lrecipe = lor[i].split(': ') # recipe in list splited by :
-        lorn.append(lrecipe[0][:-11])
-        lingr.append(lrecipe[1][:-6])
-        lop.append(lrecipe[-1])
-        i += 1
+    # # Take ingredients, staples from the user inputs
+    # ingredients, staples = # user input
+    # recipes = provide_recipes(ingredients, staples)
+    # lor = recipes.split('RECIPE NAME: ') # list of recipes
+    # # List Of Recipe Names, List of INGRedients, List Of Processes
+    # lorn, lingr, lop = [], [], []
+    # for i in range(len(lor)):
+    #     lrecipe = lor[i].split(': ') # recipe in list splited by :
+    #     lorn.append(lrecipe[0][:-11])
+    #     lingr.append(lrecipe[1][:-6])
+    #     lop.append(lrecipe[-1])
+    #     i += 1
         
-    lurl = provide_images(lorn)
+    # lurl = provide_images(lorn)
         
-    # display 3 recipes
-    if len(lor) <= 3: 
-        pass # display only 3 with no arrow buttons
-    else: 
-        # display first 3 lorn[0], lingr[0], lop[0], lorn[1], lingr[1], lop[1], lor[2] ... 
-        lorn, lingr, lop = new_lists(lorn, lingr, lop, lurl)
+    # # display 3 recipes
+    # if len(lor) <= 3: 
+    #     pass # display only 3 with no arrow buttons
+    # else: 
+    #     # display first 3 lorn[0], lingr[0], lop[0], lorn[1], lingr[1], lop[1], lor[2] ... 
+    #     lorn, lingr, lop = new_lists(lorn, lingr, lop, lurl)
         
-        if # arrow button is clicked:
-            # display first 3 recipes
-            lorn, lingr, lop = new_lists(lorn, lingr, lop, lurl)
+    #     if # arrow button is clicked:
+    #         # display first 3 recipes
+    #         lorn, lingr, lop = new_lists(lorn, lingr, lop, lurl)
 
 
 if __name__ == '__main__':
