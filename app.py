93fb3b4ab6b36c76827ea1797e4633d25c5cebe1
jkmathilda <jkmathilda627@gmail.com>
2024-01-23 17:50:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/home')
+@app.route('/home.html')
 def home():
    return render_template('home.html')
 
-# @app.route('/recipe')
-# def recipe():
-#    return render_template('recipe.html')
+@app.route('/toRecipe')
+def recipe():
+   return render_template('toRecipe.html')
 
-# @app.route('/ingredients')
-# def recipe():
-#    return render_template('ingredients.html')
+@app.route('/ingredients')
+def ingredients():
+   return render_template('ingredients.html')
 
-@app.route('/test')
-def test():
-    return 'This is Test Page!'
+@app.route('/search', methods = ["GET", "POST"])
+def search():
+    if request.method == 'POST':
+        form_data = request.form
+        val1 = form_data['inputContent']
+        val2 = form_data.get('butter')  # is_checked
+        return (f' inputContent: {val1}<br>button(butter) is checked: {val2}')
+    else:
+        # return render_template('toRecipe.html')
+        return 'This is Search Page!'
 
 if __name__ == '__main__':
     # app.run('0.0.0.0', port=5000, debug=True)
-    app.run(debug=True)
+    app.run('0.0.0.0', port=80, debug=True)
